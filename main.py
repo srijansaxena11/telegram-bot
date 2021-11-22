03d60475ccd49282e62162e0b486e221218e83a4
srijansaxena11 <34964694+srijansaxena11@users.noreply.github.com>
2021-11-22 23:21:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler, MessageHandler, Filters, InlineQueryHandler
 from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent
-import requests
-import re
+# import requests
 import logging
-from uuid import uuid4
+# from uuid import uuid4
 from telegram.utils.helpers import escape_markdown
 import random
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
-def get_eth_rate_inr():
-    print("Inside get_eth_rate_inr()")
-    eth_api = requests.get('https://rest.coinapi.io/v1/exchangerate/ETH/INR?apikey=95CBCBB3-72EB-48D3-B60D-9DDCB21F70AF').json()
-    rate = eth_api['rate']
-    return rate
+# def get_eth_rate_inr():
+#     print("Inside get_eth_rate_inr()")
+#     eth_api = requests.get('https://rest.coinapi.io/v1/exchangerate/ETH/INR?apikey=95CBCBB3-72EB-48D3-B60D-9DDCB21F70AF').json()
+#     rate = eth_api['rate']
+#     return rate
 
-def get_url():
-    contents = requests.get('https://random.dog/woof.json').json()
-    url = contents['url']
-    return url
+# def get_url():
+#     contents = requests.get('https://random.dog/woof.json').json()
+#     url = contents['url']
+#     return url
 
-def stark(update, context):
-    f = open("/etc/openhab2/scripts/tony_stark_quotes.txt","r")
-    r = random.randint(0,49)
-    lines = f.readlines()
-    quote = lines[r]
-    context.bot.send_message(chat_id=update.message.chat_id, text=quote)
+# def stark(update, context):
+#     f = open("/etc/openhab2/scripts/tony_stark_quotes.txt","r")
+#     r = random.randint(0,49)
+#     lines = f.readlines()
+#     quote = lines[r]
+#     context.bot.send_message(chat_id=update.message.chat_id, text=quote)
 
-def eth(update, context):
-    print("Inside eth()")
-    rate = get_eth_rate_inr()
-    context.bot.send_message(chat_id=update.message.chat_id, text=rate)
+# def eth(update, context):
+#     print("Inside eth()")
+#     rate = get_eth_rate_inr()
+#     context.bot.send_message(chat_id=update.message.chat_id, text=rate)
 
-def bop(update, context):
-    url = get_url()
-    chat_id = update.message.chat_id
-    context.bot.send_photo(chat_id=chat_id, photo=url)
+# def bop(update, context):
+#     url = get_url()
+#     chat_id = update.message.chat_id
+#     context.bot.send_photo(chat_id=chat_id, photo=url)
 
 def start(update, context):
     #context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
@@ -75,10 +74,10 @@ def main():
     updater = Updater('2113253226:AAHH4MMbAQieoxDQWZmpll3aJPMW6C9G4_M')
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start',start))
-    dp.add_handler(CommandHandler('eth',eth))
-    dp.add_handler(CommandHandler('bop',bop))
-    dp.add_handler(CommandHandler('stark',stark))
-    #dp.add_handler(MessageHandler(Filters.text, echo))
+#     dp.add_handler(CommandHandler('eth',eth))
+#     dp.add_handler(CommandHandler('bop',bop))
+#     dp.add_handler(CommandHandler('stark',stark))
+#     dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_handler(MessageHandler(Filters.text, sleeping))
     dp.add_handler(InlineQueryHandler(inlinequery))
     updater.start_polling()
