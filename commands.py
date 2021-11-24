9a62ebb6e6509ac1181e8f743950c27ff4e5c494
srijansaxena11 <34964694+srijansaxena11@users.noreply.github.com>
2021-11-24 18:41:05 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,9 +1,10 @@
+import os
 import requests
 
 class Commands:
   def get_eth_rate_inr():
     print("Inside Commands.get_eth_rate_inr()")
-    eth_api = requests.get(f'https://rest.coinapi.io/v1/exchangerate/ETH/INR?apikey={COINAPI_KEY}').json()
+    eth_api = requests.get(f'https://rest.coinapi.io/v1/exchangerate/ETH/INR?apikey={os.environ["COINAPI_KEY"]}').json()
     rate = eth_api['rate']
     return rate
   
@@ -13,7 +14,7 @@ class Commands:
     return url
   
   def get_current_temperature():
-    temp_api = requests.get(f'https://api.openweathermap.org/data/2.5/weather?id=1264733&appid={OPENWEATHERMAP_API_KEY}&units=metric').json()
+    temp_api = requests.get(f'https://api.openweathermap.org/data/2.5/weather?id=1264733&appid={os.environ["OPENWEATHERMAP_API_KEY"]}&units=metric').json()
     current_temperature = temp_api['main']['temp']
     feels_like_temperature = temp_api['main']['feels_like']
     return [current_temperature,feels_like_temperature]
