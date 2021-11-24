7ae8502f068ce5fca311fa7841aff50813f9c744
srijansaxena11 <34964694+srijansaxena11@users.noreply.github.com>
2021-11-24 14:10:13 +0530

    Create filters.py

---
+++ filters.py
@@ -0,0 +1,11 @@
+from telegram.ext import BaseFilter
+from telegram import Message
+
+
+class CustomFilters:
+    class _AuthorizedUserFilter(BaseFilter):
+        def filter(self, message):
+            id = message.from_user.id
+            return bool(id in [373228727])
+
+    authorized_user = _AuthorizedUserFilter()
