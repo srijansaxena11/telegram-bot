e78161694b77ee3a95dc8b89deb86e634385b462
srijansaxena11 <srijan.saxena0@gmail.com>
2021-11-27 13:21:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def test(update, context):
     conn = sqlite3.connect('teleram_bot.db')
     print ("Opened database successfully")
-    conn.execute("CREATE TABLE authorized_users
-         (user_id bigint PRIMARY KEY NOT NULL,
-         created_at datetime default now(),
-         updated_at datetime default now(),
-         lock_version int default 0;")
+    conn.execute("CREATE TABLE authorized_users (user_id bigint PRIMARY KEY NOT NULL, created_at datetime default now(), updated_at datetime default now(), lock_version int default 0;")
     print ("Table created successfully")
     conn.close()
 
